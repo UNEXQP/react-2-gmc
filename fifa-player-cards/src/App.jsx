5decf4fe86c14cf961a245e1448c850768fe86b9
UNEXQP <anfelarotimi10@gmail.com>
2024-10-25 16:40:47 +0100

    first commit

---
+++ fifa-player-cards/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayerList from './components/PlayerList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center', margin: '1rem' }}>FIFA Player Cards</h1>
+      <PlayerList />
+    </div>
+  );
+}
+
+export default App;
+
